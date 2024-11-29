8a6fa34a98f46684ac719b8916fe3a05b773b78d
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2024-11-29 12:38:38 -0400

    agregue las siguientes clases a la Carpeta Interfaces ICliente IClienteAsp IServer IServerAsp

---
+++ Shared/Interfaces/IServer.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace Shared.Interfaces;
+
+public interface IServer<T>
+{
+    public Task<T> AddObject(T type);
+    public Task<T> GetObject(int id);
+    public Task<bool> UpdateObject(T type);
+    public Task<bool> DeleteObject(int id);
+    public Task<bool> Exist(int id, string nombre);
+    public Task<List<T>> GetAllObject();
+    public Task<List<T>> GetObjectByCondition(Expression<Func<T, bool>> expression);
+}
