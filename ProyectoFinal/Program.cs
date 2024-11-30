8a50ce9afd44dc3838bdfa2de138311e01ee2f2e
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2024-11-30 03:34:02 -0400

    Inyeccion del DbContex

--- ProyectoFinal/Program.cs
+++ ProyectoFinal/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Azure;
 using ProyectoFinal.Components;
 using ProyectoFinal.Components.Account;
 using ProyectoFinal.Data;
@@ -23,9 +24,16 @@ builder.Services.AddAuthentication(options =>
     })
     .AddIdentityCookies();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+// Configure DbContext with a connection string
+builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConStr")));
+
+var storageConnection = builder.Configuration["ConnectionStrings:imagenespizza:Storage"];
+
+builder.Services.AddAzureClients(azureBuilder => {
+    azureBuilder.AddBlobServiceClient(storageConnection);
+});
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
