c9e593e7aa25af0925006bcbec12713cd2fcd9dc
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2024-11-30 03:32:28 -0400

    Agregue los modelos

--- ProyectoFinal/Data/ApplicationDbContext.cs
+++ ProyectoFinal/Data/ApplicationDbContext.cs
@@ -1,9 +1,75 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Shared.Models;
 
-namespace ProyectoFinal.Data
+namespace ProyectoFinal.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+
+    public DbSet<CategoriaProductos> CategoriaProductos { get; set; }
+    public DbSet<Productos> Productos { get; set; }
+    public DbSet<Ordenes> Ordenes { get; set; }
+    public DbSet<OrdenesDetalle> OrdenesDetalle { get; set; }
+    public DbSet<Ventas> Ventas { get; set; }
+    public DbSet<VentasDetalle> VentasDetalle { get; set; }
+    public DbSet<Estados> Estados { get; set; }
+    public DbSet<MetodoPagos> MetodoPagos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+        ConfigureGeneralModel(modelBuilder);
+        ConfigureProductosModel(modelBuilder);
+
+        // Configurar las relaciones
+        modelBuilder.Entity<Ordenes>()
+            .HasMany(o => o.OrdenesDetalle)
+            .WithOne(d => d.Orden)
+            .HasForeignKey(d => d.OrdenId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<OrdenesDetalle>()
+            .HasOne(d => d.Producto)
+            .WithMany()
+            .HasForeignKey(d => d.ProductoId);
+    }
+
+    public void ConfigureGeneralModel(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Estados>().HasData(
+             new Estados { EstadoId = 1, NombreEstado = "Pendiente" },
+             new Estados { EstadoId = 2, NombreEstado = "Preparando" },
+             new Estados { EstadoId = 3, NombreEstado = "YA ESTÁ LISTA" },
+             new Estados { EstadoId = 4, NombreEstado = "Cancelado" }
+        );
+        modelBuilder.Entity<MetodoPagos>().HasData(
+             new MetodoPagos { MetodoPagoId = 1, Nombre = "Efectivo" },
+             new MetodoPagos { MetodoPagoId = 2, Nombre = "Tarjeta" }
+        );
+        modelBuilder.Entity<CategoriaProductos>().HasData(
+             new CategoriaProductos { CategoriaId = 1, Nombre = "Pizza" },
+             new CategoriaProductos { CategoriaId = 2, Nombre = "Italian chesse" },
+             new CategoriaProductos { CategoriaId = 3, Nombre = "Bebidas" }
+             
+        );
+    }
+
+    public void ConfigureProductosModel(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Productos>().HasData(
+            new Productos { Nombre = "Gran Pepperoni", Cantidad = 50, Precio = 475, ITBIS = 85.5f, ProductoId = 1, CategoriaId = 1, Disponible = true, Descripcion = "Pizza clásica con doble pepperoni, mozzarella y salsa de tomate sobre masa artesanal.", ImagenUrl = "https://imagenespizza.blob.core.windows.net/pizza/pexels-polina-tankilevitch-4109083.jpg" },
+            new Productos { Nombre = "Jamón Deluxe", Cantidad = 50, Precio = 500, ITBIS = 90.0f, ProductoId = 2, CategoriaId = 2, Disponible = true, Descripcion = "Deliciosa pizza con jamón ahumado, queso mozzarella y un toque de albahaca fresca.", ImagenUrl = "https://imagenespizza.blob.core.windows.net/pizza/JamonDeluxe.jpg" },
+            new Productos { Nombre = "Pizza Mixta", Cantidad = 50, Precio = 450, ITBIS = 72.0f, ProductoId = 3, CategoriaId = 3, Disponible = true, Descripcion = "Una mezcla perfecta de carnes, vegetales frescos, mozzarella y orégano.", ImagenUrl = "https://imagenespizza.blob.core.windows.net/pizza/pexels-abrar-27874930.jpg" },
+            new Productos { Nombre = "Hawaiana", Cantidad = 50, Precio = 325, ITBIS = 58.5f, ProductoId = 4, CategoriaId = 1, Disponible = true, Descripcion = "Pizza dulce y salada con jamón, piña, mozzarella y salsa de tomate.", ImagenUrl = "https://imagenespizza.blob.core.windows.net/pizza/Hawaiana.jpeg" },
+            new Productos { Nombre = "Familiar Especial", Cantidad = 50, Precio = 425, ITBIS = 76.5f, ProductoId = 5, CategoriaId = 1, Disponible = true, Descripcion = "Ideal para compartir, con múltiples ingredientes: pepperoni, jamón, champiñones y más.", ImagenUrl = "https://imagenespizza.blob.core.windows.net/pizza/Familiar.webp" },
+            new Productos { Nombre = "Individual Italiana", Cantidad = 50, Precio = 400, ITBIS = 72.0f, ProductoId = 6, CategoriaId = 2, Disponible = true, Descripcion = "Pizza personal con ingredientes clásicos italianos: tomate, mozzarella y albahaca.", ImagenUrl = "https://imagenespizza.blob.core.windows.net/pizza/IndividualItaliana.jpg" },
+            new Productos { Nombre = "Vegetariana", Cantidad = 50, Precio = 500, ITBIS = 90.0f, ProductoId = 7, CategoriaId = 1, Disponible = true, Descripcion = "Cargada de vegetales frescos como pimientos, champiñones, cebolla y espinacas.", ImagenUrl = "https://imagenespizza.blob.core.windows.net/pizza/Vegetariana.jpg" },
+            new Productos { Nombre = "Barbacoa (BBQ)", Cantidad = 50, Precio = 350, ITBIS = 63.0f, ProductoId = 8, CategoriaId = 1, Disponible = true, Descripcion = "Sabor ahumado con carne de cerdo desmechada, cebolla caramelizada y salsa BBQ.", ImagenUrl = "https://imagenespizza.blob.core.windows.net/pizza/Barbacoa(BBQ).jpg" },
+            new Productos { Nombre = "Napolitana", Cantidad = 50, Precio = 375, ITBIS = 67.5f, ProductoId = 9, CategoriaId = 1, Disponible = true, Descripcion = "La tradición napolitana con tomates frescos, mozzarella y aceite de oliva.", ImagenUrl = "https://imagenespizza.blob.core.windows.net/pizza/Napolitana.jpg" },
+            new Productos { Nombre = "Cuatro Quesos", Cantidad = 50, Precio = 500, ITBIS = 90.0f, ProductoId = 10, CategoriaId = 1, Disponible = true, Descripcion = "Una mezcla de quesos irresistibles: mozzarella, parmesano, gorgonzola y ricotta.", ImagenUrl = "https://imagenespizza.blob.core.windows.net/pizza/CuatroQuesos.jpg" },
+            new Productos { Nombre = "Agua", Cantidad = 50, Precio = 20, ITBIS = 3.6f, ProductoId = 14, CategoriaId = 1, Disponible = true, Descripcion = "Botella de agua fría, ideal para acompañar cualquier pizza.", ImagenUrl = "https://imagenespizza.blob.core.windows.net/pizza/Agua.webp" },
+            new Productos { Nombre = "Coca Cola 500ml", Cantidad = 50, Precio = 50, ITBIS = 9.0f, ProductoId = 15, CategoriaId = 3, Disponible = true, Descripcion = "Refresco clásico de Coca Cola en presentación de 500ml.", ImagenUrl = "https://imagenespizza.blob.core.windows.net/pizza/Cocacola.webp" }
+        );
     }
 }
